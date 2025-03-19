38f334c2432d05d6086a3058d8f8ea22a0ed43ac
Shaun <79271767+shaunck96@users.noreply.github.com>
2025-03-19 14:05:33 -0400

    Update load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 import torch 
 
-embeddings_dataset = load_dataset(r"C:\Users\307164\Desktop\avatar_chatbot\Matthijs\cmu-arctic-xvectors.py", split="validation")
+embeddings_dataset = load_dataset(r"C:\User\Desktop\avatar_chatbot\Matthijs\cmu-arctic-xvectors.py", split="validation")
 
 speaker_embeddings = embeddings_dataset[7306]["xvector"]
 speaker_embeddings = torch.tensor(speaker_embeddings).unsqueeze(0)
